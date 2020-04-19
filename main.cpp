a7a6023252bd1a2eed61994b84600709281ff1cb
virginiasatyro <vivi_satyro@hotmail.com>
2020-04-18 22:33:16 -0300

    print walls

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 bool game_over = false;
+const int width = 20;
+const int height = 20;
+int x, y;
+int x_fruit, y_fruit;
+int score;
+enum Direction{STOP = 0, LEFT, RIGHT, UP, DOWN};
+Direction direction;
 
 void setup();
 void draw();
@@ -26,11 +33,38 @@ int main()
 void setup()
 {
     game_over = false;
+    direction = STOP;
+
+    x = width / 2; // snake in the middle
+    y = height / 2;
+
+    x_fruit = rand() % width;
+    y_fruit = rand() % height;
 }
 
 void draw()
 {
+    system("clear"); // clear console
+    
+    for(int i = 0; i < width; i++) // top wall
+        cout << "#";
+    cout << endl;
 
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            if(j == 0 || j == width - 1)
+                cout << "#";
+            else 
+                cout << " ";
+        }
+        cout << endl;
+    }
+
+    for(int i = 0; i < width; i++) //  bottom wall
+        cout << "#";
+    cout << endl;
 }
 
 void input()
